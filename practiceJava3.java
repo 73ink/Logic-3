ae19f2554906124bbc104fd4e3531de474cf2643
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-08 11:46:38 +0400

    added task-3: lucky13

--- practiceJava3.java
+++ practiceJava3.java
@@ -8,8 +8,11 @@ public class practiceJava3 {
 //        int[] nums1 = {2, 1, 2, 3, 4};
 //        System.out.println(p.countEvens(nums1));
         // task-2
-        int[] nums2 = {1, 2, 2, 1};
-        System.out.println(p.sum13(nums2));
+//        int[] nums2 = {1, 2, 2, 1};
+//        System.out.println(p.sum13(nums2));
+        // task-3
+        int[] nums3 = {0, 2, 4};
+        System.out.println(p.lucky13(nums3));
     }
 
     public int countEvens(int[] nums) {
@@ -33,6 +36,14 @@ public class practiceJava3 {
             }
         }
         return sum;
+    }
+    public boolean lucky13(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 1 || nums[i] == 3) {
+                return false;
+            }
+        }
+        return true;
     }
 
 }
