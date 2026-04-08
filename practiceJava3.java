4e4ca56f91555c8105e6f949c7290b84c2aface0
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-08 12:15:11 +0400

    added task-14: sum67

--- practiceJava3.java
+++ practiceJava3.java
@@ -223,6 +223,27 @@ public class practiceJava3 {
         }
         return max - min;
     }
+    public int sum67(int[] nums) {
+
+        int sum = 0;
+        boolean ignore = false;
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (nums[i] == 6) {
+                ignore = true;
+            }
+
+            if (ignore == false) {
+                sum = sum + nums[i];
+            }
+
+            if (nums[i] == 7 && ignore == true) {
+                ignore = false;
+            }
+        }
+        return sum;
+    }
 
 
 
