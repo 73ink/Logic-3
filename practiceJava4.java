24a6023c4ab18af3e56f838e1f6d65fa7c4e878d
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-09 11:27:40 +0400

    Task-1: only14 was added

--- practiceJava4.java
+++ practiceJava4.java
@@ -1,3 +1,18 @@
 public class practiceJava4 {
-    
+   public static void main (String[] args){
+       // task-1
+    practiceJava4 t1 = new practiceJava4();
+    int[] nums = {1, 4, 1, 4};
+    System.out.println(t1.only14(nums));   // true
+
+}
+
+public boolean only14(int[] nums){
+    for (int i = 0; i< nums.length; i++){
+        if (nums[i] != 1 && nums[i] !=4){
+        return false;
+        }
+        }
+    return true;
+    }
 }
