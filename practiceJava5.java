3fd46f8aa30aa41738cea2574f1a708ab0559339
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-14 10:02:04 +0400

    Task-5: roundSum was added

--- practiceJava5.java
+++ practiceJava5.java
@@ -10,10 +10,11 @@ public class practiceJava5 {
 //        practiceJava5 t3 = new practiceJava5();
 //        System.out.println(t3.blackjack(19, 21));
         // task-4
-        practiceJava5 t4 = new practiceJava5();
-        System.out.println(t4.loneSum(1, 2, 3));
-
-
+//        practiceJava5 t4 = new practiceJava5();
+//        System.out.println(t4.loneSum(1, 2, 3));
+        // task-5
+        practiceJava5 t5 = new practiceJava5();
+        System.out.println(t5.roundSum(12, 13, 14));
 
     }
     public boolean makeBricks(int small, int big, int goal){
@@ -87,5 +88,21 @@ public class practiceJava5 {
         }
 
         return sum;
+    }
+    public int roundSum(int a, int b, int c){
+
+        return round10(a) + round10(b) + round10(c);
+    }
+
+    public int round10(int num){
+
+        int remainder = num % 10;
+
+        if (remainder >= 5){
+            return num + (10 - remainder);
+        }
+        else{
+            return num - remainder;
+        }
     }
 }
