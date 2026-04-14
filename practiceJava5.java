734df5b94fbd6e68350dabe58132d59830a3eed4
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-14 09:55:41 +0400

    Task 2: noTeenSum was added

--- practiceJava5.java
+++ practiceJava5.java
@@ -1,8 +1,12 @@
 public class practiceJava5 {
     public static void main(String[] args){
         // task-1
-        practiceJava5 t1 = new practiceJava5();
-        System.out.println(t1.makeBricks(3, 1, 8));
+//        practiceJava5 t1 = new practiceJava5();
+//        System.out.println(t1.makeBricks(3, 1, 8));
+        // task-2
+        practiceJava5 t2 = new practiceJava5();
+        System.out.println(t2.noTeenSum(1, 2, 3));
+
 
 
     }
@@ -24,6 +28,20 @@ public class practiceJava5 {
         }
         else{
             return false;
+        }
+    }
+    public int noTeenSum(int a, int b, int c){
+
+        return fixTeen(a) + fixTeen(b) + fixTeen(c);
+    }
+
+    public int fixTeen(int n){
+
+        if (n >= 13 && n <= 19 && n != 15 && n != 16){
+            return 0;
+        }
+        else{
+            return n;
         }
     }
 }
